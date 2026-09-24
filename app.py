e295e63f61d40d52161bcc9dd19345c854fd0a6f
Christoph Weber <crazy.trash@gmx.de>
2026-09-24 12:02:49 +0200

    Add blog post creation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def load_posts():
     with open("blog_posts.json", "r") as file:
         return json.load(file)
+
+def save_posts(posts):
+    with open("blog_posts.json", "w") as file:
+        json.dump(posts, file, indent=4)
 
 @app.route("/")
 def index():
@@ -17,7 +21,28 @@ def index():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        pass
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        blog_posts = load_posts()
+
+        if blog_posts:
+            new_id = max(post["id"] for post in blog_posts) + 1
+        else:
+            new_id = 1
+
+        new_post = {
+            "id": new_id,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        return redirect(url_for("index"))
 
     return render_template("add.html")
 

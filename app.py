49a729cc983533b9c2cc6f0abfcb1a2424969995
Christoph Weber <crazy.trash@gmx.de>
2026-09-24 12:15:32 +0200

    Add blog post edit

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ def save_posts(posts):
     with open("blog_posts.json", "w") as file:
         json.dump(posts, file, indent=4)
 
+def fetch_post_by_id(post_id):
+    blog_posts = load_posts()
+
+    for post in blog_posts:
+        if post["id"] == post_id:
+            return post
+
+    return None
+
 @app.route("/")
 def index():
     blog_posts = load_posts()
@@ -45,6 +54,29 @@ def add():
         return redirect(url_for("index"))
 
     return render_template("add.html")
+
+@app.route("/update/<int:post_id>", methods=["GET", "POST"])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == "POST":
+        blog_posts = load_posts()
+
+        for blog_post in blog_posts:
+            if blog_post["id"] == post_id:
+                blog_post["author"] = request.form.get("author")
+                blog_post["title"] = request.form.get("title")
+                blog_post["content"] = request.form.get("content")
+                break
+
+        save_posts(blog_posts)
+
+        return redirect(url_for("index"))
+
+    return render_template("update.html", post=post)
 
 @app.route("/delete/<int:post_id>")
 def delete(post_id):

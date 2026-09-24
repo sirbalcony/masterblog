2085c35820fd89190fa00f36403be2993a1e08e8
Christoph Weber <crazy.trash@gmx.de>
2026-09-24 11:52:31 +0200

    Add form, form style, route to /add

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,13 +8,18 @@ def load_posts():
     with open("blog_posts.json", "r") as file:
         return json.load(file)
 
-
 @app.route("/")
 def index():
     blog_posts = load_posts()
 
     return render_template("index.html", posts=blog_posts)
 
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        pass
+
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
